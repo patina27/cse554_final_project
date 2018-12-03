49320c0400e36daadb4cc446d9b5623cde72b060
patina27 <7.pt.wang@gmail.com>
2018-12-02 20:33:21 -0600

    update

--- cse554_final_project/image_parsing.cpp
+++ cse554_final_project/image_parsing.cpp
@@ -1,25 +1,17 @@
 #include "stdafx.h"
 #include "image_parsing.h"
 
-#include<opencv2\opencv.hpp>
-#include <cstdio>
-#include <vector>
-
-using namespace std;
-using namespace cv;
-
-
-static void findCircles(const Mat& input, vector<Vec3f> &circles) {
+void findCircles(const Mat& input, vector<Vec3f> &circles, int param1, int param2, int distance=10) {
 	Mat t_img;
 	blur(input, t_img, Size(3, 3));
-	calcCircles(t_img, circles);
+	calcCircles(t_img, circles, param1, param2, distance);
 }
 
-static void calcCircles(const Mat &input, vector<Vec3f> &circles) {
-	HoughCircles(input, circles, CV_HOUGH_GRADIENT, 1, 10, 80, 65, 3, 50);
+void calcCircles(const Mat &input, vector<Vec3f> &circles, int param1, int param2, int distance=10) {
+	HoughCircles(input, circles, CV_HOUGH_GRADIENT, 1, distance, param1, param2, 1, 50);
 }
 
-static void drawCircle(Mat &input, const vector<Vec3f> &circles) {
+void drawCircles(Mat &input, const vector<Vec3f> &circles) {
 	for (int i = 0; i<circles.size(); i++) {
 		Point center(cvRound(circles[i][0]), cvRound(circles[i][1]));
 		int radius = cvRound(circles[i][2]);
--- cse554_final_project/image_parsing.h
+++ cse554_final_project/image_parsing.h
@@ -1,7 +1,13 @@
 #pragma once
+#include<opencv2\opencv.hpp>
+#include <cstdio>
+#include <vector>
 
-static void findCircles(const Mat &, vector<Vec3f> &);
 
-static void calcCircles(const Mat &, vector<Vec3f> &);
+using namespace std;
+using namespace cv;
 
-static void drawCircles(const Mat &, const vector<Vec3f> &);
+
+void findCircles(const Mat &, vector<Vec3f> &, int, int, int);
+void calcCircles(const Mat &, vector<Vec3f> &, int, int, int);
+void drawCircles(Mat &, const vector<Vec3f> &);
